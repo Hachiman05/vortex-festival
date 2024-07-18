2ba971cc83ed10d8bd4b4a7591082ba585496584
hachiman05 <bedisdofus@gmail.com git config --global user.name hachiman05git config --global user.email bedisdofus@gmail.com>
2024-07-18 12:21:20 +0200

    update 1.014

--- scripts.js
+++ scripts.js
@@ -1,18 +1,20 @@
-// Fonction d'initialisation de la carte Google Maps
-function initMap() {
-    // Coordonnées de Djerba
-    var djerba = { lat: 33.8219, lng: 10.9916 };
+function toggleMenu() {
+    const menu = document.getElementById('menu');
+    if (menu.style.display === 'block') {
+        menu.style.display = 'none';
+    } else {
+        menu.style.display = 'block';
+    }
+}
 
-    // Création de la carte centrée sur Djerba
+function initMap() {
+    var djerba = { lat: 33.8076, lng: 10.8450 };
     var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 12,
+        zoom: 10,
         center: djerba
     });
-
-    // Marqueur pour indiquer l'emplacement du festival
     var marker = new google.maps.Marker({
         position: djerba,
-        map: map,
-        title: 'VORTEX Festival'
+        map: map
     });
 }
