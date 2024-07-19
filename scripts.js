6d8bd5f88071c503754292314d3ee4522dc6e416
hachiman05 <bedisdofus@gmail.com git config --global user.name hachiman05git config --global user.email bedisdofus@gmail.com>
2024-07-19 10:08:25 +0200

    update 1.0212

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 function toggleMenu() {
-    const menu = document.querySelector('.menu');
-    menu.style.display = (menu.style.display === 'block' ? 'none' : 'block');
+    var menu = document.querySelector('.menu');
+    menu.style.display = (menu.style.display === 'block') ? 'none' : 'block';
 }
 
 document.addEventListener('click', function(event) {
-    const menu = document.querySelector('.menu');
-    const toggleButton = document.querySelector('.menu-toggle');
-    if (!menu.contains(event.target) && !toggleButton.contains(event.target)) {
+    var menu = document.querySelector('.menu');
+    var menuToggle = document.querySelector('.menu-toggle');
+    var isClickInsideMenu = menu.contains(event.target);
+    var isClickOnToggle = menuToggle.contains(event.target);
+
+    if (!isClickInsideMenu && !isClickOnToggle) {
         menu.style.display = 'none';
     }
 });
