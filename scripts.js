58a3973bf8ae1065b5733537287289605be976c1
hachiman05 <bedisdofus@gmail.com git config --global user.name hachiman05git config --global user.email bedisdofus@gmail.com>
2024-07-19 13:39:34 +0200

    update 1.0217

--- scripts.js
+++ scripts.js
@@ -1,16 +1,13 @@
-function toggleMenu() {
-    var menu = document.querySelector('nav ul');
-    menu.classList.toggle('active');
-}
+// script.js
 
-// Initialize the map
+// Function to initialize the Google Map
 function initMap() {
-    var location = { lat: 33.8076, lng: 10.8472 }; // Djerba, Tunisia
-    var map = new google.maps.Map(document.getElementById('map'), {
+    const location = { lat: 33.8076, lng: 10.8515 };
+    const map = new google.maps.Map(document.getElementById('map'), {
         zoom: 10,
         center: location
     });
-    var marker = new google.maps.Marker({
+    const marker = new google.maps.Marker({
         position: location,
         map: map
     });
