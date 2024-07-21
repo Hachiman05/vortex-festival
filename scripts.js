aed7fcf8c2e69d5a15cbde63479875a65ba63a55
hachiman05 <bedisdofus@gmail.com git config --global user.name hachiman05git config --global user.email bedisdofus@gmail.com>
2024-07-21 13:33:30 +0200

    update 1.03

--- scripts.js
+++ scripts.js
@@ -1,16 +1,14 @@
 // Fonction pour initialiser la carte Google Maps
 function initMap() {
     const mapOptions = {
-        center: { lat: 33.8838, lng: 10.8368 }, // Coordonnées de Djerba comme exemple
+        center: { lat: 33.8838, lng: 10.8368 },
         zoom: 14,
         mapTypeId: google.maps.MapTypeId.ROADMAP,
-        disableDefaultUI: true // Désactiver les contrôles par défaut
+        disableDefaultUI: true
     };
 
-    // Création de la carte
     const map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
-    // Ajout d'un marqueur comme exemple
     new google.maps.Marker({
         position: mapOptions.center,
         map: map,
@@ -21,8 +19,7 @@ function initMap() {
 // Fonction pour afficher ou masquer le menu
 function toggleMenu() {
     const nav = document.querySelector('nav');
-    const isActive = nav.classList.contains('active');
-    nav.classList.toggle('active', !isActive);
+    nav.classList.toggle('active');
 }
 
 // Écouteur d'événement pour fermer le menu lors du clic en dehors
@@ -35,5 +32,5 @@ document.addEventListener('click', (event) => {
     }
 });
 
-// Assurer que la fonction `initMap` est appelée lorsque la page est chargée
+// Appel de initMap lors du chargement de la page
 window.addEventListener('load', initMap);
